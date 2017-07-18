ff3eec145b0a215091ad0dcc7241a0d4e7b7f880
d <d>
2017-07-18 09:54:18 +0800

    edit

--- src/main/java/com/di/kit/FilterUtil.java
+++ src/main/java/com/di/kit/FilterUtil.java
@@ -9,18 +9,30 @@ import java.util.List;
 public class FilterUtil {
 	/**
 	 * 获取数组1和数组2对象的交集部分
-	 * @param list1 数组1
-	 * @param list2 数组2
-	 * @param key 判断对象一致的key
+	 * 
+	 * @param list1
+	 *            数组1
+	 * @param list2
+	 *            数组2
+	 * @param key
+	 *            判断对象一致的key
 	 * @return
 	 */
-	public static <T> List<T> andSetTogether(List<T> list1, List<T> list2, String key) {
+	public static <T> List<T> andSetTogether(List<T> list1, List<T> list2, String... keys) {
 		List<T> tmps = new ArrayList<>();
 		for (T t1 : list1) {
-			Object v1 = ClassUtil.getFieldValueByGetMethod(key, t1);
 			for (T t2 : list2) {
-				Object v2 = ClassUtil.getFieldValueByGetMethod(key, t2);
-				if (v1.equals(v2)) {
+				int i = 0;
+				for (String key : keys) {
+					Object v1 = ClassUtil.getFieldValueByGetMethod(key, t1);
+					Object v2 = ClassUtil.getFieldValueByGetMethod(key, t2);
+					if (v1.equals(v2)) {
+						i++;
+					} else {
+						break;
+					}
+				}
+				if (i == keys.length) {
 					tmps.add(t1);
 					break;
 				}
@@ -28,32 +40,42 @@ public class FilterUtil {
 		}
 		return tmps;
 	}
+
 	/**
 	 * 获取数组1减去与数组2交集部分
-	 * @param list1 数组1
-	 * @param list2 数组2
-	 * @param key 判断对象一致的key
+	 * 
+	 * @param list1
+	 *            数组1
+	 * @param list2
+	 *            数组2
+	 * @param key
+	 *            判断对象一致的key
 	 * @return
 	 */
-	public static <T> List<T> miniusSet(List<T> list1, List<T> list2, String key) {
+	public static <T> List<T> miniusSet(List<T> list1, List<T> list2, String... keys) {
 		List<T> tmps = new ArrayList<>();
 		for (T t1 : list1) {
-			boolean b=false;
-			Object v1 = ClassUtil.getFieldValueByGetMethod(key, t1);
+			boolean b = false;
 			for (T t2 : list2) {
-				Object v2 = ClassUtil.getFieldValueByGetMethod(key, t2);
-				b=v1.equals(v2);
-				if (b) {
-					break;
+				int i = 0;
+				for (String key : keys) {
+					Object v1 = ClassUtil.getFieldValueByGetMethod(key, t1);
+					Object v2 = ClassUtil.getFieldValueByGetMethod(key, t2);
+					if (!v1.equals(v2)) {
+						break;
+					} else {
+						i++;
+					}
+				}
+				if (i == keys.length) {
+					b = true;
 				}
 			}
-			if(b){
-				continue;
-			}else{
+			if (!b) {
 				tmps.add(t1);
 			}
 		}
 		return tmps;
 	}
-	
+
 }
