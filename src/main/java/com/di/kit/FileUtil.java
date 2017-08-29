474e11727a581c210f33b6d505d1c70e589c1bcd
d <d>
2017-08-29 11:40:26 +0800

    nio file read

--- src/main/java/com/di/kit/FileUtil.java
+++ src/main/java/com/di/kit/FileUtil.java
@@ -3,12 +3,18 @@ package com.di.kit;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 
 /**
  * @author di
@@ -116,6 +122,54 @@ public class FileUtil {
 	    }
 	}
 	return null;
+    }
+
+    public static byte[] readBytesByNio(String path) throws IOException {
+	File f = new File(path);
+	if (!f.exists()) {
+	    throw new FileNotFoundException(path);
+	}
+	FileChannel channel = null;
+	FileInputStream fs = null;
+	try {
+	    fs = new FileInputStream(f);
+	    channel = fs.getChannel();
+	    ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
+	    while ((channel.read(byteBuffer)) > 0) {
+	    }
+	    return byteBuffer.array();
+	} catch (IOException e) {
+	    throw e;
+	} finally {
+	    try {
+		channel.close();
+		fs.close();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    @SuppressWarnings("resource")
+    public static byte[] readBytesLarge(String filename) throws IOException {
+	FileChannel fc = null;
+	try {
+	    fc = new RandomAccessFile(filename, "r").getChannel();
+	    MappedByteBuffer byteBuffer = fc.map(MapMode.READ_ONLY, 0, fc.size()).load();
+	    byte[] result = new byte[(int) fc.size()];
+	    if (byteBuffer.remaining() > 0) {
+		byteBuffer.get(result, 0, byteBuffer.remaining());
+	    }
+	    return result;
+	} catch (IOException e) {
+	    throw e;
+	} finally {
+	    try {
+		fc.close();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
     }
 
     private static String getRealPath(String relativePath) {
