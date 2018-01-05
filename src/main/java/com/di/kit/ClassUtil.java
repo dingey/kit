583d10c6261d71be6eadf904416d1560167dbe12
d <d>
2018-01-05 19:12:02 +0800

    a

--- src/main/java/com/di/kit/ClassUtil.java
+++ src/main/java/com/di/kit/ClassUtil.java
@@ -20,8 +20,8 @@ import java.util.Map;
 public class ClassUtil {
 	public static boolean isUserClass(Class<?> c) {
 		return !c.isPrimitive() && c != Byte.class && c != Short.class && c != Integer.class && c != Long.class
-				&& c != Double.class && c != Float.class && c != Character.class && c != Boolean.class
-				&& c != Date.class && c != java.sql.Date.class && !c.isInterface() && !c.isEnum()
+				&& c != Double.class && c != Float.class && c != Character.class && c != String.class
+				&& c != Boolean.class && c != Date.class && c != java.sql.Date.class && !c.isInterface() && !c.isEnum()
 				&& (c instanceof Object) && c != Object.class && c != Class.class;
 	}
 
