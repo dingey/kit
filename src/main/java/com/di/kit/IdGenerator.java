5fde698632cd854de216b585c8bafc8ae75b5d80
d <d>
2018-05-27 14:32:34 +0800

    id

--- src/main/java/com/di/kit/IdGenerator.java
+++ src/main/java/com/di/kit/IdGenerator.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -12,13 +13,17 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author d
  */
 public class IdGenerator {
-	private static final SimpleDateFormat df = new SimpleDateFormat("yyMMddHHmmss");
+	private static final SimpleDateFormat df = new SimpleDateFormat(
+			"yyMMddHHmmss");
 	private static AtomicLong lastTime = getMinute();
 	private static final long maxCount = 999998;
 	private static AtomicInteger count = new AtomicInteger(1);
-	static {
+	private static AtomicBoolean run = new AtomicBoolean(false);
+	static void start() {
+		run.getAndSet(true);
 		Calendar date = Calendar.getInstance();
-		date.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE), date.get(Calendar.HOUR),
+		date.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH),
+				date.get(Calendar.DATE), date.get(Calendar.HOUR),
 				date.get(Calendar.MINUTE), date.get(Calendar.SECOND));
 		long period = 1 * 1000;
 		Timer t = new Timer();
@@ -36,6 +41,9 @@ public class IdGenerator {
 	public static final long nextId() throws RuntimeException {
 		if (count.get() > maxCount) {
 			throw new RuntimeException("产生的id超过最大限制");
+		}
+		if (!run.get()) {
+			start();
 		}
 		return getId();
 	}
