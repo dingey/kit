60c1aeec29949fa0842a21af4ae7c7f97f7e5488
dingey <123929187@qq.com>
2018-09-25 09:23:12 +0800

    Update XmlUtil.java

--- src/main/java/com/di/kit/XmlUtil.java
+++ src/main/java/com/di/kit/XmlUtil.java
@@ -11,6 +11,8 @@ import javax.xml.bind.Unmarshaller;
  * @author d
  */
 public class XmlUtil {
+	private static DocumentBuilderFactory factory;
+	
 	public static <T> String toXml(T t) {
 		StringWriter sw = new StringWriter();
 		try {
@@ -35,4 +37,47 @@ public class XmlUtil {
 		}
 		return t;
 	}
+
+	public static Map<String, String> fromXml(String xml) {
+		try {
+			DocumentBuilderFactory factory = safeFactory();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document parse = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+			Element element = parse.getDocumentElement();
+			Map<String, String> m = new LinkedHashMap<>();
+			if (parse.hasChildNodes()) {
+				NodeList nodes = element.getChildNodes();
+				for (int i = 0; i < nodes.getLength(); i++) {
+					Node node = nodes.item(i);
+					if (node.getNodeType() == 1) {
+						m.put(node.getNodeName(), node.getTextContent());
+					}
+				}
+			}
+			return m;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static DocumentBuilderFactory safeFactory() {
+		if (factory == null) {
+			factory = DocumentBuilderFactory.newInstance();
+			try {
+				String FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
+				factory.setFeature(FEATURE, true);
+				FEATURE = "http://xml.org/sax/features/external-general-entities";
+				factory.setFeature(FEATURE, false);
+				FEATURE = "http://xml.org/sax/features/external-parameter-entities";
+				factory.setFeature(FEATURE, false);
+				FEATURE = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+				factory.setFeature(FEATURE, false);
+				factory.setXIncludeAware(false);
+				factory.setExpandEntityReferences(false);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return factory;
+	}	
 }
